c70967419986d9bff30ab1c5844e214202fb795e
Snailclimb <koushuangbwcx@163.com>
2019-02-26 12:37:40 +0800

    the distance 8 km and the waiting time is 2 minute

--- taxi-fee/spec/main-spec.js
+++ taxi-fee/spec/main-spec.js
@@ -7,5 +7,11 @@ describe('taxi fee', function () {
         let minute=0;
         let money=calculate(distance,minute);
         expect(money).to.be.equal(6);
-       });
+    });
+    it("the distance 8 km and the waiting time is 2 minute,the starting price is 11",function(){
+        let distance=8;
+        let minute=2;
+        let money=calculate(distance,minute);
+        expect(money).to.be.equal(11);
+    });
 });
