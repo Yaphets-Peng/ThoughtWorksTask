1a78ca2887224311f6fbfc6ce99b130ef1fd2b36
Snailclimb <koushuangbwcx@163.com>
2019-02-28 14:05:04 +0800

    the distance is less than 0 km and the waiting time is less than 0 minute, print:the data is illega

--- taxi-fee/main/main.js
+++ taxi-fee/main/main.js
@@ -1,9 +1,11 @@
-module.exports = function calculate(distance,minute) {
-    if(distance<=2){
-      return  Math.round(6+minute*0.25);
-    }else if(2<distance && distance<=8){
-     return Math.round(6+(distance-2)*0.8+minute*0.25);
-    }else{
-      return Math.round(6+(8-2)*0.8+(distance-8)*1.2+minute*0.25);
-    }
+module.exports = function calculate(distance, minute) {
+  if (distance < 0 || minute < 0) {
+      return ("The data is illegal!");
+  } else if (0<distance  && distance <= 2) {
+      return Math.round(6 + minute * 0.25);
+  } else if (2 < distance && distance <= 8) {
+      return Math.round(6 + (distance - 2) * 0.8 + minute * 0.25);
+  } else {
+      return Math.round(6 + (8 - 2) * 0.8 + (distance - 8) * 1.2 + minute * 0.25);
+  }
 };
--- taxi-fee/spec/main-spec.js
+++ taxi-fee/spec/main-spec.js
@@ -2,6 +2,12 @@ var expect = require('chai').expect;
 const calculate = require('../main/main');
 
 describe('taxi fee', function () {
+    it("the distance is less than 0 km and the waiting time is less than 0 minute, print:the data is illegal",function(){
+        let distance=-2;
+        let minute=-1;
+        let money=calculate(distance,minute);
+        expect(money).to.be.equal("The data is illegal!");
+    });
     it("the distance is less than 2 km and the waiting time is 0 minute, the starting price is 6",function(){
         let distance=1.5;
         let minute=0;
